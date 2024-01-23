2549b13ab4516dff18fb9ee7244eb77f708290c7
Pilar Rust <mdrust@nuskin.com>
2024-01-23 13:39:03 -0700

    Took some headers out

--- api/add-log.js
+++ api/add-log.js
@@ -2,15 +2,6 @@ import { sql } from '@vercel/postgres';
  
 export default async function handler(request, response) {
   try {
-    response.setHeader('Access-Control-Allow-Credentials', true)
-    response.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    response.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
     const { symbol1, symbol2, rounds } = request.body;
     if(!symbol1 && !symbol2 && ! rounds){
         return response.status(400).json({error: "You must provide all column values(symbo1,symbol2,rounds)"})

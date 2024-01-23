eb0dec53f45a881f0840c0d0e0744a538874c492
Pilar Rust <mdrust@nuskin.com>
2024-01-23 15:09:27 -0700

    Return group id after insert

--- api/add-log.js
+++ api/add-log.js
@@ -8,7 +8,7 @@ export default async function handler(request, response) {
         return response.status(400).json({error: "You must provide all column values(symbo1,symbol2,rounds)"})
     }
    
-    const result = await sql`INSERT INTO game_log (symbol1, symbol2, rounds, start_time, end_time) VALUES (${symbol1}, ${symbol2},${rounds},CURRENT_TIMESTAMP,NULL);`;
+    const result = await sql`INSERT INTO game_log (symbol1, symbol2, rounds, start_time, end_time) VALUES (${symbol1}, ${symbol2},${rounds},CURRENT_TIMESTAMP,NULL) RETURNING game_id;`;
     return response.status(200).json({result});
   } catch (error) {
     return response.status(500).json({ error });

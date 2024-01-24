76e43a70a20b29e3c12725bacd026617062cf210
Pilar Rust <mdrust@nuskin.com>
2024-01-23 18:39:38 -0700

    Order logs by group id

--- api/get-logs.js
+++ api/get-logs.js
@@ -2,7 +2,7 @@ import { sql } from '@vercel/postgres';
  
 export default async function handler(request, response) {
     response.setHeader('Access-Control-Allow-Origin', '*')
-    const gameLog = await sql`SELECT * FROM game_log;`;
+    const gameLog = await sql`SELECT * FROM game_log ORDER BY game_id`;
     return response.status(200).json({ gameLog });
  
   
